701cffce7f3d91c0350428cda7738404e8fa2902
wuhongru <1425636954@qq.com>
2023-04-19 12:36:06 +0800

    load

---
+++ 01_配置颜色、字体和形状/Color.kt
@@ -0,0 +1,14 @@
+package com.heiko.bloom.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val pink100 = Color(0xFFFFF1F1)
+val pink900 = Color(0xFF3F2C2C)
+val white = Color(0xFFFFFFFF)
+val white850 = Color(0xD9FFFFFF)
+val gray = Color(0xFF232323)
---
+++ 01_配置颜色、字体和形状/Shape.kt
@@ -0,0 +1,14 @@
+package com.heiko.bloom.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
+
+val small = RoundedCornerShape(4.dp)
+val medium = RoundedCornerShape(24.dp)
